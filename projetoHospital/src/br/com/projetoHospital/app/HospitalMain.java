3891e2fc919ca73c622649ca5606842f0c3fda45
opet <opet@RB-LAB207A-03.Academico.edu>
2019-08-05 21:18:21 -0300

    Ajuste classe main

--- projetoHospital/src/br/com/projetoHospital/app/HospitalMain.java
+++ projetoHospital/src/br/com/projetoHospital/app/HospitalMain.java
@@ -5,6 +5,7 @@ package br.com.projetoHospital.app;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import br.com.projetoHospital.model.Administrativo;
@@ -76,7 +77,8 @@ public class HospitalMain {
 		String nome;
 		String sobrenome;
 		String dtNascimento;
-		long cpf;
+		long cpf = 0;
+		boolean validarRepository;
 		
 		Scanner tc = new Scanner(System.in);
 		
@@ -91,30 +93,47 @@ public class HospitalMain {
 		dtNascimento = tc.next();
 		
 		System.out.println("Digite o cpf :");
-		cpf = tc.nextInt();
-		
-		if(key.equalsIgnoreCase("MEDICO")) {
-			System.out.println("Digite o Especialidade :");
-			String especialidade = tc.next();
-			Medico med = new Medico(nome, sobrenome, dtNascimento, cpf, especialidade);
-			
-			repository.add(med);
-			
-		}else if(key.equalsIgnoreCase("ENFERMEIRO")) {
-			System.out.println("Digite o carga horaria :");
-			int cargaHoraria = tc.nextInt();
-			Enfermeiro enf = new Enfermeiro(nome, sobrenome, dtNascimento, cpf, cargaHoraria);
-			
-			repository.add(enf);
-		}else if(key.equalsIgnoreCase("ADMINISTRATIVO")) {
-			System.out.println("Digite o valor fixo :");
-			long valorFixo = tc.nextLong();
-			
-			Administrativo adm = new Administrativo(nome, sobrenome, dtNascimento, cpf, valorFixo);
-			
-			repository.add(adm);
+		try {
+			cpf = tc.nextInt();
+		}catch(InputMismatchException ex) {
+			System.out.println("CAMPO ACEITA APENAS NUMERO TENTE NOVAMENTE");
 		}
 		
+		
+		if(validarRepository(repository)) {
+			System.out.println("LIMITE PARA CADASTRO 10");
+		}else {
+		
+			if(key.equalsIgnoreCase("MEDICO")) {
+				System.out.println("Digite o Especialidade :");
+				String especialidade = tc.next();
+				Medico med = new Medico(nome, sobrenome, dtNascimento, cpf, especialidade);
+				
+				repository.add(med);
+				
+			}else if(key.equalsIgnoreCase("ENFERMEIRO")) {
+				System.out.println("Digite o carga horaria :");
+				int cargaHoraria = tc.nextInt();
+				Enfermeiro enf = new Enfermeiro(nome, sobrenome, dtNascimento, cpf, cargaHoraria);
+				
+				repository.add(enf);
+			}else if(key.equalsIgnoreCase("ADMINISTRATIVO")) {
+				System.out.println("Digite o valor fixo :");
+				long valorFixo = tc.nextLong();
+				
+				Administrativo adm = new Administrativo(nome, sobrenome, dtNascimento, cpf, valorFixo);
+				
+				repository.add(adm);
+			}
+		}	
+	}
+	
+	public static boolean validarRepository(ArrayList<Pessoa> repository) {
+		if(repository.size() >= 10) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 }
